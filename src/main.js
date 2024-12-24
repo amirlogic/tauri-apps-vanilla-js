1cbbb150d0efa6d6d7338a96e099e28638f540b6
Amir <amir.hac@outlook.com>
2024-12-24 18:40:34 +0100

    switch to bootstrap

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
 const { invoke } = window.__TAURI__.core;
 const { exists, BaseDirectory, readTextFile } = window.__TAURI__.fs;
 const { open, message } = window.__TAURI__.dialog;
+const { load } = window.__TAURI__.store;
 
+let openedFile
+
+const targetEl = 'markdown'
+
+let history = []
 
 let greetInputEl;
 let greetMsgEl;
+
+async function storeFileName(fname){
+
+  const store = await load('store.json', { autoSave: false });
+
+  await store.set('lastfile', { value: fname });
+
+  await store.save();
+}
+
+async function getStoreData(){
+
+  const store = await load('store.json', { autoSave: false });
+
+  return await store.get('lastfile').value
+}
+
+function showHistory(){
+
+
+}
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
@@ -21,16 +48,16 @@ async function openMD() {
     });
 
     let mdcontent = await readTextFile(filename)
-
-    //const converter = new showdown.Converter(),
       
     let html = marked.parse(mdcontent);
 
-    document.getElementById('markdown').innerHTML = html
+    document.getElementById(targetEl).innerHTML = html
 
     //document.getElementById('debug').textContent = await path.appDataDir()
 
     document.getElementById('moreinfo').textContent = filename
+
+    await storeFileName(filename)
 
   }
   catch(err){
@@ -50,7 +77,7 @@ async function viewMD() {
       //text      = '# hello, markdown!',
       html      = converter.makeHtml(mdcontent);
 
-    document.getElementById('markdown').innerHTML = html
+    document.getElementById(targetEl).innerHTML = html
     document.getElementById('debug').textContent = "test" //filename
   }
   catch(err){
@@ -62,16 +89,27 @@ async function viewMD() {
 
 async function testDialog(){
 
-  await message('Just a test', { title: 'Tauri', kind: 'info' });
+  let stored = getStoreData()
+
+  if(typeof(stored) == "string"){
+
+    await message(`Stored filename: ${getStoreData()}`, { title: 'Tauri', kind: 'info' });
+  }
+  else if(typeof(stored) == "object"){
+
+    await message(`Stored filename: ${JSON.stringify(stored)}`, { title: 'Tauri', kind: 'info' });
+  }
+  else{
+
+    await message(`Error: Could not get stored data`, { title: 'Tauri', kind: 'error' });
+  }
+  
 }
 
 window.addEventListener("DOMContentLoaded", () => {
 
   greetInputEl = document.querySelector("#greet-input");
   greetMsgEl = document.querySelector("#greet-msg");
-  
-  //document.getElementById('moreinfo').innerHTML = JSON.stringify(window)
-  //openMD()
 
   document.getElementById('testdialog').addEventListener("click", (e) => {
 
@@ -79,16 +117,38 @@ window.addEventListener("DOMContentLoaded", () => {
     openMD()
   })
 
+  document.getElementById('nav-about').addEventListener("click", async (e) => {
+
+    await message(`Created by Amir Hachaichi\nUses marked`, { title: 'About', kind: 'info' });
+
+  })
+
+  document.getElementById('nav-history').addEventListener("click", async (e) => {
+
+
+
+    document.getElementById(targetEl).innerHTML = ""
+
+    //await message(`Created by Amir Hachaichi\nUses marked`, { title: 'About', kind: 'info' });
+
+  })
+
+  document.getElementById('nav-clear').addEventListener("click", (e) => {
+
+    document.getElementById(targetEl).innerHTML = ""
+
+  })
+
   document.getElementById('mdfile').addEventListener("change", (e) => {
 
-    document.getElementById('filedata').textContent = document.getElementById('mdfile').value  //JSON.stringify( document.getElementById('mdfile').files)
+    document.getElementById('filedata').textContent = document.getElementById('mdfile').value 
   });
 
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     //greet();
-    viewMD()
-    
+    //viewMD()
+    testDialog()
     
   });
 });
